198971b0c35baa9fb8921e167e76725313abf55a
30817 <129130009+junjunjunjunjunjunjunjunjun@users.noreply.github.com>
2023-04-13 01:29:08 +0000

    condition_ifelse.c/mission1.c

---
+++ .ccls-cache/@home@runner@GOOD/mission1.c
@@ -0,0 +1,12 @@
+
+main(){
+  int a;
+  printf("정수를입력: ");
+  scanf("%d", &a);
+
+  if(a%2==0){
+    printf("짝수");
+  }
+  else
+    printf("홀수");
+}
---
+++ condition_ifelse.c
@@ -0,0 +1,17 @@
+
+// main(){
+//   int a=5;
+
+//   if(a>3){
+//     printf("A\n");
+//   }
+//   else
+//     printf("B\n");
+//   printf("c\n");
+
+//   if(a>8) printf("D\n");
+
+//   else printf("E\n");
+
+//   printf("F\n");
+// }
