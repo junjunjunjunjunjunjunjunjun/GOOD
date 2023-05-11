608e0af6557c23f2547f14c4510cefef8a29aaa8
junjunjunjunjunjunjunjunjun <129130009+junjunjunjunjunjunjunjunjun@users.noreply.github.com>
2023-05-11 01:19:18 +0000

    switch 추가

---
+++ mission2.c
@@ -0,0 +1,18 @@
+
+// main(){
+//   int x;
+//   printf("점수를 입력하세요.");
+//   scanf("%d", &x);
+
+//   if(x>=90)
+//     printf("A학점");
+//   else if(90>x>=80)
+//     printf("B학점");
+//   else if(80>x>=70)
+//     printf("C학점");
+//   else if(70>x>=60)
+//     printf("D학점");
+//   else if(60>x)
+//     printf("F학점");
+//   printf("점수조사 끝!");
+// }
---
+++ mission3.c
@@ -0,0 +1,13 @@
+
+// main(){
+//   int x;
+//   int y;
+//   printf("두개의 숫자를 입력하세요:");
+//   scanf("%d", &x);
+//   scanf("%d", &y);
+
+//   if(x>=y)
+//     printf("%d", x);
+//   else if(x<=y)
+//     printf("%d", y);
+// }
